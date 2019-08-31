d543ab269d9cd7d1e80e538fdb825d35a44c74ec
XiaoZhuMarket <35296289+ZhuXiao0796@users.noreply.github.com>
2019-08-31 17:12:43 +0800

    Init repo

---
+++ Histroy/Assets/InterfaceTest/Scripts/InterfaceTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.InterfaceTest
+{
+    public class InterfaceTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+    public abstract class AbstractBase { }
+        
+    public interface MyInterface
+    {
+
+    }
+}
---
+++ Histroy/Assets/OnDestroyTest/Scripts/OnDestroyTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDestroyTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Destroy(gameObject);
+    }
+
+    private void OnDisable()
+    {
+        Debug.Log("销毁之前的操作");
+    }
+}
