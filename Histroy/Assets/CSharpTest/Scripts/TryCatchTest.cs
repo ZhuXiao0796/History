0a255ab3c3e8325ef29e555930b45b2920bc4739
XiaoZhuMarket <35296289+ZhuXiao0796@users.noreply.github.com>
2019-09-02 19:19:46 +0800

    Try Catch Test

---
+++ Histroy/Assets/CSharpTest/Scripts/TryCatchTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryCatchTest : MonoBehaviour
+{
+	public GameObject A;
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            if (i>=0)
+            {
+                return;
+            }
+            Debug.Log(A.name);
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e.Message);
+            throw;
+        }
+        finally
+        {
+            Debug.Log("Finally part");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Histroy/Assets/Coroutine/Scripts/WhileCoroutineTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.CoroutineTest
+{
+    /// <summary>
+    /// 当在协程内部用 While 进行判断，在外部打断  While 内的条件，则 While 中止。
+    /// </summary>
+    public class WhileCoroutineTest : MonoBehaviour
+    {
+        private bool status;
+        // Start is called before the first frame update
+        void Start()
+        {
+            status = true;
+            StartCoroutine(WhileTest());
+            Invoke("ChangeStatus", 5f);
+        }
+
+        private void ChangeStatus()
+        {
+            status = false;
+        }
+
+       private IEnumerator WhileTest()
+        {
+            while (status)
+            {
+                Debug.Log(11111);
+                yield return new WaitForSeconds(1f);
+            }
+        }
+    }
+}
