2cec45e70e88f296399553f354b432f767aeb541
XiaoZhuGit <2535751003@qq.com>
2019-09-03 14:00:57 +0800

    Physics Test
    测试了 胶囊型 物理检测的 实际使用范围

---
+++ Histroy/Assets/Physics/Scripts/PhysicsTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.PhysicsTest
+{
+    /// <summary>
+    ///  测试了 物理射线中的 圆柱体射线
+    /// </summary>
+    public class PhysicsTest : MonoBehaviour
+    {
+        public GameObject end;
+        public  Vector3 endPos = new Vector3(0,5,0);
+        // Start is called before the first frame update
+        void Start()
+        {
+           //var colliders = Physics.
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var colliders = Physics.OverlapCapsule(transform.position, endPos, 5f);
+            if (colliders!=null)
+            {
+                foreach (var item in colliders)
+                {
+                    Debug.Log(item.name);
+                }
+            }
+        }
+    }
+}
