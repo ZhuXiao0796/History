4a8a6e8ce801c2ab52fd19f3424b34b696934942
XiaoZhuGit <2535751003@qq.com>
2019-09-03 14:07:48 +0800

    Transform Test
    测试了变换组件的几个固定值

---
+++ Histroy/Assets/TransformTest/Scripts/TransformTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.TransformTest
+{
+    /// <summary>
+    /// 测试了 好奇很久的 当前坐标的前后左右的实际值
+    /// </summary>
+    public class TransformTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.position = new Vector3(10,10,10);
+            Debug.Log(transform.up);
+            Debug.Log(transform.forward);
+            Debug.Log(transform.right);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
