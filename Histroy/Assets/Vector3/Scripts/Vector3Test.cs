0841b35473ee65c139b199eb159fc529653b9ff8
XiaoZhuGit <2535751003@qq.com>
2019-09-04 20:28:44 +0800

    Vector Test
    测试 Vector3 类型的 API

---
+++ Histroy/Assets/Vector3/Scripts/Vector3Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 v1 = new Vector3(5,5,5);
+        Vector3 v2 = new Vector3(7,7,7);
+
+        //求两个向量的夹角
+        var angle = Vector3.Angle(v1,v2);
+
+        //求两个向量的距离
+        var dis = Vector3.Distance(v1,v2);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
