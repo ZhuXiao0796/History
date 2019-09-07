df782dd8ed257ec9ecb7e9bb33e2b2da5d4aa271
XiaoZhuGit <2535751003@qq.com>
2019-09-07 16:50:06 +0800

    Transform Test
    测试 在 Vector3.MoveTowards 本地坐标和世界坐标的区别

---
+++ Histroy/Assets/TransformTest/Scripts/DirectionTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.TransformTest
+{
+    /// <summary>
+    /// 测试 Transform 角度对 Vector3.MoveToward 的影响
+    /// </summary>
+    public class DirectionTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            StartCoroutine(MoveTowardsTest());
+        }
+
+        private IEnumerator MoveTowardsTest()
+        {
+            while (true)
+            {
+                //transform.position = Vector3.MoveTowards(transform.position,transform.TransformPoint( transform.position + transform.forward*5),Time.deltaTime*1);
+                transform.position = Vector3.MoveTowards(transform.position,transform.forward + transform.position,Time.deltaTime*1);
+                
+                yield return null;
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
