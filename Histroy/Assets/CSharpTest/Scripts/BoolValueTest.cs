c085b3362de141da19c8769b793d639c3929d7c2
XiaoZhuGit <2535751003@qq.com>
2019-09-12 13:17:45 +0800

    Daily Commit

---
+++ Histroy/Assets/CSharpTest/Scripts/BoolValueTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.CSharpTest
+{
+    /// <summary>
+    /// 用来测试 If 方法中放方法的结果
+    /// </summary>
+    public class BoolValueTest : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (IfTest())
+            {
+                Debug.Log("True");
+            }
+        }
+
+        private bool IfTest()
+        {
+            return true;
+        }
+     
+    }
+}
