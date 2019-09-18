9e320600c522ccdebba907e4d58709cdfdeab715
XiaoZhuGit <2535751003@qq.com>
2019-09-18 20:48:10 +0800

    Daily Commit
    测试和修改

---
+++ Histroy/Assets/Component/TransformTest/Scripts/LookAtOnlyY.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace History.ComponentTest.TransformTest
+{
+    public class LookAtOnlyY : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.LookAt(new Vector3(5,5,5));
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
